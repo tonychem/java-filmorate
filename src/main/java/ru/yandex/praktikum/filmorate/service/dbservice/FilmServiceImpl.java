c5e77a951a36912a1721b378bf9f8af81d2be6e9
Anatoly <tonychem@yandex.ru>
2022-07-12 20:59:00 +0300

    feat: full code cleanup

--- src/main/java/ru/yandex/praktikum/filmorate/service/dbservice/FilmServiceImpl.java
+++ src/main/java/ru/yandex/praktikum/filmorate/service/dbservice/FilmServiceImpl.java
@@ -2,8 +2,6 @@ package ru.yandex.praktikum.filmorate.service.dbservice;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
-import ru.yandex.praktikum.filmorate.exception.NoSuchFilmException;
-import ru.yandex.praktikum.filmorate.exception.NoSuchUserException;
 import ru.yandex.praktikum.filmorate.model.Film;
 import ru.yandex.praktikum.filmorate.model.User;
 import ru.yandex.praktikum.filmorate.service.FilmService;
@@ -37,38 +35,12 @@ public class FilmServiceImpl implements FilmService {
 
     @Override
     public void removeLike(long filmId, long userId) {
-        checkFilm(filmId);
-        checkUserExists(userId);
-        if (checkLike(filmId, userId)) { // TODO: тавтология?
-            filmLikesDAO.removeLike(filmId, userId);
-        }
+        filmLikesDAO.removeLike(filmId, userId);
     }
 
     @Override
     public List<Film> getMostLikedFilms(int limit) {
         List<Long> filmIds = filmLikesDAO.mostLikedFilms(limit);
         return filmIds.stream().map(filmStorage::filmById).collect(Collectors.toList());
-    }
-
-    private boolean checkFilm(long filmId) {
-        Film film = filmStorage.filmById(filmId);
-        if (film == null) {
-            throw new NoSuchFilmException(String.format("Фильм с id = %d отсутствует", filmId));
-        } else {
-            return true;
-        }
-    }
-
-    private boolean checkUserExists(long userId) {
-        User user = userStorage.userById(userId);
-        if (user == null) {
-            throw new NoSuchUserException(String.format("Пользователя с id = %d не существует", userId));
-        } else {
-            return true;
-        }
-    }
-
-    private boolean checkLike(long filmId, long userId) {
-        return filmLikesDAO.checkKeyExists(filmId, userId);
     }
 }
